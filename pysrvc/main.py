a6e99e3c01d3cbc31d37bb9f9c9065dc0d1c0e7b
Radu Stefan <radu.stefan@dynatrace.com>
2022-12-02 11:54:13 +0000

    changed for exercise clarity

--- pysrvc/main.py
+++ pysrvc/main.py
@@ -1,54 +1,38 @@
-import json
 import random
-from datetime import datetime
 from flask import Flask, request, make_response
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry import trace, propagate
-from opentelemetry.trace.status import Status, StatusCode
+# Example exercise - add Flask auto-instrumentation
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
-from fib import fibonacci
-from otel import otel_setup, requests_metric, proc_duration_metric
+from utils import process
+from otel import ot
 
 
 app = Flask("Py-Flask-App")
+# Example exercise - add Flask auto-instrumentation
 FlaskInstrumentor.instrument_app(app)
 
 
 @app.route("/quote", methods=["GET"])
 def quote():
-    requests_metric().add(1, {"request": "/quote"})
+    ot.metrics["requests_count"].add(1, {"request": "/quote"})
     trace.set_span_in_context(trace.get_current_span(), propagate.extract(request.headers))
-    process(random.randint(0, 22))
+    process(random.randint(0, 25))
     print("done")
     return make_response({}, 200)
 
 
 @app.route("/calc", methods=["GET"])
 def calc():
-    requests_metric().add(1, {"request": "/calc"})
+    ot.metrics["requests_count"].add(1, {"request": "/calc"})
+    # Example exercise - create a Span, set it in Context
     trace.set_span_in_context(trace.get_current_span(), propagate.extract(request.headers))
-    process(random.randint(0, 22))
+    process(random.randint(0, 25))
     print("done")
     return make_response({}, 200)
 
 
-def process(n: int) -> int:
-    with trace.get_tracer("custom").start_as_current_span("process") as span:
-        span.set_attribute("n", n)
-        try:
-            span.add_event("Calculating Fibonnaci", {"n": n})
-            start = datetime.now()
-            f = fibonacci(n)
-            duration = (datetime.now().timestamp() - start.timestamp())*1000
-            proc_duration_metric().record(duration, {"number": n})
-            return f
-        except Exception as e:
-            span.record_exception(e)
-            span.set_status(Status(StatusCode.ERROR, e))
-
-
 def main():
-    otel_setup()
     app.run(host='0.0.0.0', port=8090, debug=False)
 
     
--- pysrvc/otel.py
+++ pysrvc/otel.py
@@ -1,3 +1,5 @@
+import json
+import psutil
 from opentelemetry import trace, metrics
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.semconv.resource import ResourceAttributes
@@ -5,66 +7,101 @@ from opentelemetry.sdk.trace import TracerProvider, sampling
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.metrics import CallbackOptions, Observation
 from dynatrace.opentelemetry.metrics.export import configure_dynatrace_metrics_export
 
 
-"""Returns a resource describing this application."""
-def get_resource_props():
-    # Basic resource details
-    resource = {
-        ResourceAttributes.SERVICE_NAME: "pysrvc svc on port 8090",
-        ResourceAttributes.SERVICE_VERSION: "v1.0.0",
-        "environment": "hotday"
-    }
-    # Get OneAgent's topology metadata and add to resource
-    try:
-        data = ''
-        with open("dt_metadata_e617c525669e072eebe3d0f08212e8f2.json") as f:
-            data = json.load(open(f.read()))
-        resource.update(data)
-    except:
-        pass    
-
-    return resource
+"""Gathers CPU usage, reported as an OpenTelemtry observation"""
+def get_cpu_usage(_: CallbackOptions):
+    for (number, percent) in enumerate(psutil.cpu_percent(percpu=True)):
+        attributes = {"cpu_number": str(number)}
+        yield Observation(percent, attributes)
 
 
-"""Sets up OpenTelemetry Trace & Metrics export"""
-def otel_setup():
-    resource_props = get_resource_props()
-    # Set up trace export
-    tracer_provider = TracerProvider(
-        sampler=sampling.ALWAYS_ON,
-        resource=Resource(resource_props)
-    )
-    tracer_provider.add_span_processor(
-        BatchSpanProcessor(OTLPSpanExporter(
-            endpoint="http://localhost:14499/otlp/v1/traces"
+"""A class for controlling custom OpenTelemetry behavior"""
+class CustomOpenTelemetry():
+    def __init__(self):
+        self.resource_props = self.get_resource_props()
+        self.setup_exporters()
+        self.metrics = {}
+        self.meter = metrics.get_meter("perform-hot")
+        self.meter.create_observable_gauge(
+            callbacks=[get_cpu_usage],
+            name="cpu_usage",
+            description="CPU Usage per processor, as percentage",
+            unit="1"
+        )
+        self.create_counter_instrument(
+            "requests_count",
+            "Counts the number of requests to the service"
+        )
+        # Example exercise - create a histogram metric instrument
+        self.create_histogram_instrument(
+            "process_duration",
+            "Duration of Fibonacci calculation, in milliseconds",
+            "ms"
+        )
+
+    """Returns a resource properties describing this application."""
+    def get_resource_props(self):
+        # Basic resource details
+        resource = {
+            ResourceAttributes.SERVICE_NAME: "pysrvc svc on port 8090",
+            ResourceAttributes.SERVICE_VERSION: "v1.0.0",
+            "environment": "hotday"
+        }
+        # Get OneAgent's topology metadata and add to resource
+        try:
+            data = ''
+            with open("dt_metadata_e617c525669e072eebe3d0f08212e8f2.json") as f:
+                data = json.load(open(f.read()))
+            resource.update(data)
+        except:
+            pass    
+
+        return resource
+
+    """Sets up OpenTelemetry Trace & Metrics export"""
+    def setup_exporters(self):
+        # Set up trace export
+        tracer_provider = TracerProvider(
+            sampler=sampling.ALWAYS_ON,
+            resource=Resource(self.resource_props)
+        )
+        tracer_provider.add_span_processor(
+            BatchSpanProcessor(OTLPSpanExporter(
+                endpoint="http://localhost:14499/otlp/v1/traces"
+            ))
+        )
+        trace.set_tracer_provider(tracer_provider)
+
+        # Set up metrics export
+        metrics.set_meter_provider(MeterProvider(
+            metric_readers=[
+                configure_dynatrace_metrics_export(
+                    export_dynatrace_metadata=True,
+                    prefix="perform.opentelemetry.hot",
+                    default_dimensions=self.resource_props
+                )
+            ]
         ))
-    )
-    trace.set_tracer_provider(tracer_provider)
 
-    # Set up metrics export
-    metrics.set_meter_provider(MeterProvider(
-        metric_readers=[
-            configure_dynatrace_metrics_export(
-                export_dynatrace_metadata=True,
-                prefix="custom.opentelemetry",
-                default_dimensions=resource_props
-            )
-        ]
-    ))
+    """Creates a synchronous counter instrument with given details"""
+    def create_counter_instrument(self, name: str, description: str):
+        self.metrics[name] = self.meter.create_counter(
+            name=name, 
+            description=description, 
+            unit="1"
+        )
 
+    # Example exercise - create a histogram metric instrument
+    """Creates a synchronous histogram instrument with given details"""
+    def create_histogram_instrument(self, name: str, description: str, unit: str):
+        self.metrics[name] = self.meter.create_histogram(
+            name=name,
+            description=description,
+            unit=unit
+        )
 
-def requests_metric():
-    return metrics.get_meter("custom").create_counter(
-        name="requests_count", 
-        description="Number of custom requests", 
-        unit="1"
-    )
-
-def proc_duration_metric():
-    return metrics.get_meter("custom").create_histogram(
-        name="process_duration",
-        description="Duration of Fibonacci processing in Milliseconds",
-        unit="ms"
-    )
+# Sets everything up and can be reused anywhere in the code
+ot = CustomOpenTelemetry()
